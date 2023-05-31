a2c148bb8d86f7152dcc635f7c3a751cd1f2a010
Pedro Santos <112526196+pedrosantos2006@users.noreply.github.com>
2023-05-30 21:20:14 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,19 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
     <?php
 
     // Array é uma lista onde pode adicionar e procurar itens adicionados
 
-    //Criando um array com o nome de futas e os valores maçã banana laaranja e morango
+    //Criando um array com o nome de frutas e os valores maçã, banana, laranja, e morango
 
     $frutas = array("maçã", "banana", "laranja", "morango");
 
@@ -19,7 +21,7 @@
 
     print_r($frutas);
 
-    //Inserindo um novo valor [abcaxi] dentro do array $frutas:
+    //Inserindo um novo valor [abacaxi] dentro do array $frutas:
 
     $frutas[4] = "abacaxi";
     echo "<br>";
@@ -35,7 +37,7 @@
 
     //removendo array valor maça
 
-    unset ($frutas[0]);
+    unset($frutas[0]);
     echo "<br>";
     print_r($frutas);
 
@@ -47,10 +49,11 @@
     print_r($frutas);
 
     // utilizando o laço de repetição para exibir todos os items da array $frutas
-echo "<br>";
-    foreach($frutas as $fruta){
+    echo "<br>";
+    foreach ($frutas as $fruta) {
         echo $fruta . "<br>";
     }
     ?>
 </body>
+
 </html>
